72e353ea2dc0b14e58ffe5f7be98c639c37605b5
Arttu Jokinen <arttu.jokinen@qvik.fi>
2021-08-30 16:30:04 +0300

    some styling tweaks

--- app/src/main/java/com/example/ratingsapp/features/main/HomeScreen.kt
+++ app/src/main/java/com/example/ratingsapp/features/main/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.ratingsapp.features.main
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
@@ -12,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -51,12 +53,17 @@ fun HomeScreen(mainVm:MainViewModel) {
 @ExperimentalCoilApi
 @Composable
 fun GameListItem(item: Game){
-    Card(modifier = Modifier
-        .height(300.dp)
-        .width(200.dp), shape = RoundedCornerShape(10)) {
+    Card(
+        shape = RoundedCornerShape(10),
+        modifier = Modifier
+        .height(250.dp).width(170.dp).padding(4.dp).clickable {
+            //TODO NAVIGATION TO DETAILS
+        }) {
         Image(
             painter = rememberImagePainter(item.imageLink),
             contentDescription = "My content description",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
     }
 }
@@ -75,7 +82,7 @@ fun itemPreview(@PreviewParameter(GameListProvider::class) games: List<Game>){
 @ExperimentalFoundationApi
 @Composable
 fun HomeGameList(games: List<Game>){
-    LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 175.dp)) {
+    LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 150.dp)) {
         items(games) { game ->
             GameListItem(item = game)
         }
